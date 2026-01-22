620bef39533084c9130ab8a2ccb57e85f86a99b9
salauddin <salauddinn@users.noreply.github.com>
2026-01-22 20:28:32 +0530

    Updated draggableNode.js

--- frontend/src/draggableNode.js
+++ frontend/src/draggableNode.js
@@ -3,20 +3,25 @@
 export const DraggableNode = ({ type, label }) => {
     const onDragStart = (event, nodeType) => {
       const appData = { nodeType }
+       event.target.style.cursor = 'grabbing';
       event.dataTransfer.setData('application/reactflow', JSON.stringify(appData));
       event.dataTransfer.effectAllowed = 'move';
     };
 
     const onDragEnd = (event) => {
       event.preventDefault();
+             event.target.style.cursor = 'grab');
     };
 
     return (
       <div
-        className="draggable-node"
+        className=`draggable-node ${type} `
         onDragStart={(event) => onDragStart(event, type)}
         onDragEnd={onDragEnd}
         draggable={true}
+        style={{
+          cursor:'drag'
+        }}
       >
           <span>{label}</span>
       </div>
