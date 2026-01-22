1c2b73e00d6dc47989a9d2107e33babd8f6d0aa2
salauddin <salauddinn@users.noreply.github.com>
2026-01-22 19:05:36 +0530

    Updated inputNode.js

--- frontend/src/nodes/inputNode.js
+++ frontend/src/nodes/inputNode.js
@@ -1,7 +1,7 @@
 // inputNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const InputNode = ({ id, data }) => {
   const [currName, setCurrName] = useState(data?.inputName || id.replace('customInput-', 'input_'));
@@ -15,33 +15,27 @@ export const InputNode = ({ id, data }) => {
     setInputType(e.target.value);
   };
 
+  const outputs = [
+    { id: 'value' }
+  ];
+
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Input</span>
-      </div>
-      <div>
-        <label>
-          Name:
-          <input 
-            type="text" 
-            value={currName} 
-            onChange={handleNameChange} 
-          />
-        </label>
-        <label>
-          Type:
-          <select value={inputType} onChange={handleTypeChange}>
-            <option value="Text">Text</option>
-            <option value="File">File</option>
-          </select>
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-value`}
-      />
-    </div>
+    <BaseNode id={id} title="Input" outputs={outputs}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={currName}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Type:
+        <select value={inputType} onChange={handleTypeChange}>
+          <option value="Text">Text</option>
+          <option value="File">File</option>
+        </select>
+      </label>
+    </BaseNode>
   );
 }
--- frontend/src/nodes/textNode.js
+++ frontend/src/nodes/textNode.js
@@ -1,7 +1,7 @@
 // textNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const TextNode = ({ id, data }) => {
   const [currText, setCurrText] = useState(data?.text || '{{input}}');
@@ -10,26 +10,20 @@ export const TextNode = ({ id, data }) => {
     setCurrText(e.target.value);
   };
 
+  const outputs = [
+    { id: 'output' }
+  ];
+
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Text</span>
-      </div>
-      <div>
-        <label>
-          Text:
-          <input 
-            type="text" 
-            value={currText} 
-            onChange={handleTextChange} 
-          />
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-output`}
-      />
-    </div>
+    <BaseNode id={id} title="Text" outputs={outputs}>
+      <label>
+        Text:
+        <input
+          type="text"
+          value={currText}
+          onChange={handleTextChange}
+        />
+      </label>
+    </BaseNode>
   );
 }
